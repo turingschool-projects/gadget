76d43ff5fb473b46a21c2d7912e7c2b34e790a0e
Daniel Shin <shin.daniel.d@gmail.com>
2017-02-28 19:10:44 -0700

    env variable

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,7 +1,7 @@
 class GithubService
 
   def self.github(username)
-    response = Faraday.get("https://api.github.com/users/#{username}?access_token=ee24cad06ee4becebc076ef33834b5d41ebd3fef")
+    response = Faraday.get("https://api.github.com/users/#{username}?access_token=#{ENV['access_token']}")
     parse_json(response.body)
   end
 
